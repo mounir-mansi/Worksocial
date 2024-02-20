824731416e60a23ce419a7d9b022740a7d60c617
mounir-mansi <114388491+mounir-mansi@users.noreply.github.com>
2024-02-20 13:27:12 +0100

    Update Hostname.js

--- frontend/src/HostnameConnect/Hostname.js
+++ frontend/src/HostnameConnect/Hostname.js
@@ -2,7 +2,7 @@ const settings = {
   devRunMode: 100,
   withConsole: false,
 };
-const hostname = `https://worksocial.onrender.com`;
+const hostname = `https://worksocial-yrhr.onrender.com`;
 
 // Utilisez export direct au lieu de module.exports
 export { settings, hostname };
